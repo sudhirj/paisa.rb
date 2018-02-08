0021a2c765526a262adee08160c3a630095bafba
Sudhir Jonathan <sudhir.j@gmail.com>
2018-02-08 14:05:16 +0530

    Rubocop formatting

--- lib/paisa.rb
+++ lib/paisa.rb
@@ -2,29 +2,31 @@ require 'paisa/version'
 
 module Paisa
   UNITS = {
-      1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
-      6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 0 => nil
-  }
+    1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
+    6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 0 => nil
+  }.freeze
 
-  def self.combine_unit (base, k)
+  def self.combine_unit(base, k)
     [base, UNITS[k]].compact.join(' ')
   end
 
+  private_class_method :combine_unit
+
   TENS = {
-      1 => {
-          1 => 'eleven', 2 => 'twelve', 3 => 'thirteen', 4 => 'fourteen', 5 => 'fifteen',
-          6 => 'sixteen', 7 => 'seventeen', 8 => 'eighteen', 9 => 'nineteen', 0 => 'ten'
-      },
-      2 => Hash.new {|_, k| combine_unit('twenty', k)},
-      3 => Hash.new {|_, k| combine_unit('thirty', k)},
-      4 => Hash.new {|_, k| combine_unit('forty', k)},
-      5 => Hash.new {|_, k| combine_unit('fifty', k)},
-      6 => Hash.new {|_, k| combine_unit('sixty', k)},
-      7 => Hash.new {|_, k| combine_unit('seventy', k)},
-      8 => Hash.new {|_, k| combine_unit('eighty', k)},
-      9 => Hash.new {|_, k| combine_unit('ninety', k)},
-      0 => UNITS
-  }
+    1 => {
+      1 => 'eleven', 2 => 'twelve', 3 => 'thirteen', 4 => 'fourteen', 5 => 'fifteen',
+      6 => 'sixteen', 7 => 'seventeen', 8 => 'eighteen', 9 => 'nineteen', 0 => 'ten'
+    },
+    2 => Hash.new { |_, k| combine_unit('twenty', k) },
+    3 => Hash.new { |_, k| combine_unit('thirty', k) },
+    4 => Hash.new { |_, k| combine_unit('forty', k) },
+    5 => Hash.new { |_, k| combine_unit('fifty', k) },
+    6 => Hash.new { |_, k| combine_unit('sixty', k) },
+    7 => Hash.new { |_, k| combine_unit('seventy', k) },
+    8 => Hash.new { |_, k| combine_unit('eighty', k) },
+    9 => Hash.new { |_, k| combine_unit('ninety', k) },
+    0 => UNITS
+  }.freeze
 
   def self.format(paise, precision: 2)
     little_endian = paise.to_s.reverse.chars
@@ -43,26 +45,24 @@ module Paisa
     '₹'
   end
 
-
   def self.words(paise)
     little_endian = paise.to_s.reverse.chars
     paise = [little_endian.shift(2).map(&:to_i).reverse]
     hundreds = [little_endian.shift(3).map(&:to_i).reverse]
-    others = little_endian.each_slice(2).map {|p| p.map(&:to_i)}.map(&:reverse)
+    others = little_endian.each_slice(2).map { |p| p.map(&:to_i) }.map(&:reverse)
 
     [paise, hundreds, others].flatten(1).reject(&:empty?).reduce([]) do |memo, section|
-      label = ['paise', 'rupees', 'thousand', 'lakh', 'crore'][memo.size]
+      label = %w[paise rupees thousand lakh crore][memo.size]
       case section.size
-        when 1
-          memo << [UNITS[section[0]], label].join(' ')
-        when 2
-          memo << [TENS[section[0]][section[1]], label].join(' ')
-        when 3
-          memo << ["#{UNITS[section[0]]} hundred and", TENS[section[1]][section[2]], label].join(' ')
-        else
-          # do nothing
+      when 1
+        memo << [UNITS[section[0]], label].join(' ')
+      when 2
+        memo << [TENS[section[0]][section[1]], label].join(' ')
+      when 3
+        memo << ["#{UNITS[section[0]]} hundred and", TENS[section[1]][section[2]], label].join(' ')
+      else
+        # do nothing
       end
     end.to_a.reverse.join(', ')
   end
 end
-
