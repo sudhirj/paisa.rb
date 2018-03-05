9ab94c1ff4479302e05d8ad8547042c05fe00411
maknahar <maknahar@live.in>
2018-03-06 02:54:05 +0530

    Removed use of multiline map

--- lib/paisa.rb
+++ lib/paisa.rb
@@ -35,40 +35,40 @@ module Paisa
 
   HINDI_MAPPING = {
     one: 'एक', two: 'दो', three: 'तीन', four: 'चार', five: 'पांच', six: 'छह', seven: 'सात', zero: 'शून्य',
-    eight: 'आठ', nine: 'नौ', ten: 'दस', eleven: 'ग्यारह', twelve: 'बारह', thirteen: 'तेरह', fourteen: 'चौदह', fifteen: 'पंद्रह',
-    sixteen: 'सोलह', seventeen: 'सत्रह', eighteen: 'अठारह', nineteen: 'उन्नीस', hundred: 'सौ', thousand: 'हजार',
-    lakh: 'लाख', crore: 'करोड़', rupees: 'रुपये', paise: 'पैसे', and: 'और',
+    eight: 'आठ', nine: 'नौ', ten: 'दस', eleven: 'ग्यारह', twelve: 'बारह', thirteen: 'तेरह', fourteen: 'चौदह',
+    fifteen: 'पंद्रह', sixteen: 'सोलह', seventeen: 'सत्रह', eighteen: 'अठारह', nineteen: 'उन्नीस',
+    hundred: 'सौ', thousand: 'हजार', lakh: 'लाख', crore: 'करोड़', rupees: 'रुपये', paise: 'पैसे', and: 'और',
     twenty: {
-      one: 'इकीस', two: 'बाईस', three: 'तेइस', four: 'चौबीस', five: 'पच्चीस', six: 'छब्बीस', seven: 'सताइस',
-      eight: 'अट्ठाइस', nine: 'उनतीस', zero: 'बीस'
+      one: 'इकीस', two: 'बाईस', three: 'तेइस', four: 'चौबीस', five: 'पच्चीस',
+      six: 'छब्बीस', seven: 'सताइस', eight: 'अट्ठाइस', nine: 'उनतीस', zero: 'बीस'
     },
     thirty: {
-      one: 'इकतीस', two: 'बतीस', three: 'तैंतीस', four: 'चौंतीस', five: 'पैंतीस', six: 'छतीस', seven: 'सैंतीस', eight: 'अड़तीस',
-      nine: 'उनतालीस', zero: 'तीस'
+      one: 'इकतीस', two: 'बतीस', three: 'तैंतीस', four: 'चौंतीस', five: 'पैंतीस',
+      six: 'छतीस', seven: 'सैंतीस', eight: 'अड़तीस', nine: 'उनतालीस', zero: 'तीस'
     },
     forty: {
-      one: 'इकतालीस', two: 'बयालीस', three: 'तैतालीस', four: 'चवालीस', five: 'पैंतालीस', six: 'छयालिस', seven: 'सैंतालीस',
-      eight: 'अड़तालीस', nine: 'उनचास', zero: 'चालीस'
+      one: 'इकतालीस', two: 'बयालीस', three: 'तैतालीस', four: 'चवालीस', five: 'पैंतालीस',
+      six: 'छयालिस', seven: 'सैंतालीस', eight: 'अड़तालीस', nine: 'उनचास', zero: 'चालीस'
     },
     fifty: {
-      one: 'इक्यावन', two: 'बावन', three: 'तिरपन', four: 'चौवन', five: 'पचपन', six: 'छप्पन', seven: 'सतावन', eight: 'अठावन',
-      nine: 'उनसठ', zero: 'पचास'
+      one: 'इक्यावन', two: 'बावन', three: 'तिरपन', four: 'चौवन', five: 'पचपन',
+      six: 'छप्पन', seven: 'सतावन', eight: 'अठावन', nine: 'उनसठ', zero: 'पचास'
     },
     sixty: {
-      one: 'इकसठ', two: 'बासठ', three: 'तिरसठ', four: 'चौंसठ', five: 'पैंसठ', six: 'छियासठ', seven: 'सड़सठ',
-      eight: 'अड़सठ', nine: 'उनहतर', zero: 'साठ'
+      one: 'इकसठ', two: 'बासठ', three: 'तिरसठ', four: 'चौंसठ', five: 'पैंसठ',
+      six: 'छियासठ', seven: 'सड़सठ', eight: 'अड़सठ', nine: 'उनहतर', zero: 'साठ'
     },
     seventy: {
-      one: 'इकहतर', two: 'बहतर', three: 'तिहतर', four: 'चौहतर', five: 'पचहतर', six: 'छिहतर', seven: 'सतहतर',
-      eight: 'अठहतर', nine: 'उन्नासी', zero: 'सत्तर'
+      one: 'इकहतर', two: 'बहतर', three: 'तिहतर', four: 'चौहतर', five: 'पचहतर',
+      six: 'छिहतर', seven: 'सतहतर', eight: 'अठहतर', nine: 'उन्नासी', zero: 'सत्तर'
     },
     eighty: {
-      one: 'इक्यासी', two: 'बयासी', three: 'तिरासी', four: 'चौरासी', five: 'पचासी', six: 'छियासी', seven: 'सतासी',
-      eight: 'अट्ठासी', nine: 'नवासी', zero: 'अस्सी'
+      one: 'इक्यासी', two: 'बयासी', three: 'तिरासी', four: 'चौरासी', five: 'पचासी',
+      six: 'छियासी', seven: 'सतासी', eight: 'अट्ठासी', nine: 'नवासी', zero: 'अस्सी'
     },
     ninety: {
-      one: 'इक्यानवे', two: 'बानवे', three: 'तिरानवे', four: 'चौरानवे', five: 'पचानवे', six: 'छियानवे', seven: 'सतानवे',
-      eight: 'अट्ठानवे', nine: 'निन्यानवे', zero: 'नब्बे'
+      one: 'इक्यानवे', two: 'बानवे', three: 'तिरानवे', four: 'चौरानवे', five: 'पचानवे',
+      six: 'छियानवे', seven: 'सतानवे', eight: 'अट्ठानवे', nine: 'निन्यानवे', zero: 'नब्बे'
     }
   }.freeze
 
@@ -107,16 +107,15 @@ module Paisa
 
   def self.hindi(english)
     h = nil
-    english.split(', ').map do |x|
-      x.split.map do |k|
-        if HINDI_MAPPING[k.to_sym].is_a? Hash
-          h = HINDI_MAPPING[k.to_sym]
+    english.split(', ').each_with_object([]) do |x, memo|
+      memo << x.split.map(&:to_sym).each_with_object([]) do |k, mem|
+        if HINDI_MAPPING[k].is_a? Hash
+          h = HINDI_MAPPING[k]
         else
-          hindi_number = h.nil? ? HINDI_MAPPING[k.to_sym] : (h[k.to_sym].nil? ?
-            [h[:zero], HINDI_MAPPING[k.to_sym]].join(' ') : h[k.to_sym])
+          hindi_number = h.nil? ? HINDI_MAPPING[k] : h[k] || [h[:zero], HINDI_MAPPING[k]].join(' ')
           h = nil
         end
-        hindi_number
+        mem << hindi_number
       end.compact.join(' ')
     end.join(', ')
   end
